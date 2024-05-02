162f8370df16f1906e7b7fae235ede2dd74ee009
Falk <91922000+Flex-Falk@users.noreply.github.com>
2024-05-02 16:38:44 +0200

    RunningMat should now correctly identify steps

--- ESP32 Code/RunningMat/RunningMat.ino
+++ ESP32 Code/RunningMat/RunningMat.ino
@@ -7,23 +7,30 @@
 
 #define DEBOUNCE_TIME  50                     // the debounce time in millisecond, increase this time if it still chatters
 
+#define PRESS_THRESHOLD 4                     // Threshold for number of state changes to confirm a press; used to finetune
+                                              //higher: lower sensitivity aka more force for an input
+                                              //lower: higher sensitivity aka less force for an input
+
 // Variables for the Left Mat:
 int lastSteadyState_Left = LOW;               // the previous steady state from the input pin
 int lastFlickerableState_Left = LOW;          // the previous flickerable state from the input pin
 int currentState_Left;                        // the current reading from the input pin
 unsigned long lastDebounceTime_Left = 0;      // the last time the output pin was toggled
+int pressCounter_Left = 0;                    // variable for press counter
 
 // Variables for the Middle Mat:
 int lastSteadyState_Middle = LOW;             // the previous steady state from the input pin
 int lastFlickerableState_Middle  = LOW;       // the previous flickerable state from the input pin
 int currentState_Middle ;                     // the current reading from the input pin
 unsigned long lastDebounceTime_Middle  = 0;   // the last time the output pin was toggled
+int pressCounter_Middle = 0;                  // variable for press counter
 
 // Variables for the Right Mat:
 int lastSteadyState_Right = LOW;              // the previous steady state from the input pin
 int lastFlickerableState_Right  = LOW;        // the previous flickerable state from the input pin
 int currentState_Right ;                      // the current reading from the input pin
 unsigned long lastDebounceTime_Right  = 0;    // the last time the output pin was toggled
+int pressCounter_Right = 0;                   // variable for press counter
 
 
 void setup() {
@@ -48,42 +55,49 @@ void loop() {
   if (currentState_Left != lastFlickerableState_Left) {       // If the switch/button changed, due to noise or pressing:
     lastDebounceTime_Left = millis();                         // reset the debouncing timer
     lastFlickerableState_Left = currentState_Left;            // save the the last flickerable state
+    pressCounter_Left++;                                      // Increment counter for Left mat  
   }
 
   if (currentState_Middle != lastFlickerableState_Middle) {   // If the switch/button changed, due to noise or pressing:
     lastDebounceTime_Middle = millis();                       // reset the debouncing timer
     lastFlickerableState_Middle = currentState_Middle;        // save the the last flickerable state
+    pressCounter_Middle++;                                    // Increment counter for Middle mat  
   }
 
   if (currentState_Right != lastFlickerableState_Right) {     // If the switch/button changed, due to noise or pressing:
     lastDebounceTime_Right = millis();                        // reset the debouncing timer
     lastFlickerableState_Right = currentState_Right;          // save the the last flickerable state
+    pressCounter_Right++;                                     // Increment counter for Right mat
   }
 
   if ((millis() - lastDebounceTime_Left) > DEBOUNCE_TIME) {           // whatever the reading is at, it's been there for longer than the debounce delay, so take it as the actual current state:
-    if(lastSteadyState_Left == HIGH && currentState_Left == LOW)      // if the mat state has changed:
+    if(pressCounter_Left >= PRESS_THRESHOLD){
+      lastSteadyState_Left = currentState_Left;
+      pressCounter_Left = 0;                                  // Reset counter after confirmation
       Serial.print("Left:1");
-    else if(lastSteadyState_Left == LOW && currentState_Left == HIGH)
-      Serial.print("Left:0");
-
-    lastSteadyState_Left = currentState_Left;                         // save the the last steady state
+    } else{
+      pressCounter_Left = 0;                                  // Reset counter after confirmation
+    }
   }
 
-  if ((millis() - lastDebounceTime_Middle) > DEBOUNCE_TIME) {         // whatever the reading is at, it's been there for longer than the debounce delay, so take it as the actual current state:
-    if(lastSteadyState_Middle == HIGH && currentState_Middle == LOW)  // if the mat state has changed:
+
+  if ((millis() - lastDebounceTime_Middle) > DEBOUNCE_TIME) {           // whatever the reading is at, it's been there for longer than the debounce delay, so take it as the actual current state:
+    if(pressCounter_Middle >= PRESS_THRESHOLD){
+      lastSteadyState_Middle = currentState_Middle;
+      pressCounter_Middle = 0;                                  // Reset counter after confirmation
       Serial.print("Middle:1");
-    else if(lastSteadyState_Middle == LOW && currentState_Middle == HIGH)
-      Serial.print("Middle:0");
-
-    lastSteadyState_Middle = currentState_Middle;                     // save the the last steady state
+    } else{
+      pressCounter_Middle = 0;                                  // Reset counter after confirmation
+    }
   }
 
-  if ((millis() - lastDebounceTime_Right) > DEBOUNCE_TIME) {          // whatever the reading is at, it's been there for longer than the debounce delay, so take it as the actual current state:
-    if(lastSteadyState_Right == HIGH && currentState_Right == LOW)    // if the mat state has changed:
+  if ((millis() - lastDebounceTime_Right) > DEBOUNCE_TIME) {           // whatever the reading is at, it's been there for longer than the debounce delay, so take it as the actual current state:
+    if(pressCounter_Right >= PRESS_THRESHOLD){
+      lastSteadyState_Right = currentState_Right;
+      pressCounter_Right = 0;                                  // Reset counter after confirmation
       Serial.print("Right:1");
-    else if(lastSteadyState_Right == LOW && currentState_Right == HIGH)
-      Serial.print("Right:0");
-
-    lastSteadyState_Right = currentState_Right;                       // save the the last steady state
+    } else{
+      pressCounter_Right = 0;                                  // Reset counter after confirmation
+    }
   }
 }
