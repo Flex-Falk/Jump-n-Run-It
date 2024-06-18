1f5718e47405d6004f16c8f66618b7ac75f36d1f
xEatos <eatos@gmx.de>
2024-06-18 16:19:39 +0200

    fix broken data

--- Data Analysis/scatterplot.py
+++ Data Analysis/scatterplot.py
@@ -3,32 +3,50 @@ import glob
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import os
+import fnmatch
 
 from fakeData import fakeImuData
 
-files = ["imu_jump.csv"]
-template_path = r"..\Unity Code\{file}"
+files = []
+file = "export_2.csv"
 
 value = []
-def plot_imu_data(df):
+
+def list_paths(folder='.', pattern='*', case_sensitive=False, subfolders=False):
+    """Return a list of the file paths matching the pattern in the specified
+    folder, optionally including files inside subfolders.
+    """
+    match = fnmatch.fnmatchcase if case_sensitive else fnmatch.fnmatch
+    walked = os.walk(folder) if subfolders else [next(os.walk(folder))]
+    return [os.path.join(root, f)
+            for root, dirnames, filenames in walked
+            for f in filenames if match(f, pattern)]
+def plot_imu_data(df, title):
     fig, axes = plt.subplots(2, 3, figsize=(15, 10), picker=True)
 
     y_vars = df.columns.tolist()[1:]
 
     for ax, y_var in zip(axes.flatten(), y_vars):
-        sns.scatterplot(data=df, x="delta", y=y_var, ax=ax, hue="label")
+        sns.scatterplot(data=df, x="Time", y=y_var, ax=ax, hue="Action")
         ax.set_title(f'delta vs {y_var}')
 
-    def onPick(e):
-        print("pickevent scatter", e)
-    fig.canvas.mpl_connect('pick_event', onPick)
     plt.tight_layout()
+    plt.title(label=title)
     plt.show()
 
 def plot_each_seq():
-    paths = [template_path.format(file=f) for f in files]
-    for path in paths:
-        pdFrame = pd.read_csv(path, sep=",");
+    files = list_paths(pattern='*.csv')
+    for file in files:
+        print(file)
+        pdFrame = pd.read_csv(file, sep=",");
+        groupedDf = pdFrame.groupby(pdFrame.Type)
+        df_gyro = groupedDf.get_group("Gyro")
+        df_accel = groupedDf.get_group("Accel")
+        print(pdFrame)
+        print(df_gyro)
+        print(df_accel)
+        plot_imu_data(df_gyro, file + " - gyro")
+        plot_imu_data(df_accel, file + " - accel")
 
-plot_imu_data(fakeImuData(20))
-print(value)
+plot_each_seq()
